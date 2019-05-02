2a04b2f5739a2ef541773ed08c9e341588e750d9
qlixes <qlixess@gmail.com>
2019-05-02 07:09:39 +0700

    - re-code from scratch

--- src/Banks.php
+++ src/Banks.php
@@ -2,14 +2,35 @@
 
 namespace qlixes\Bank;
 
+use qlixes\Bank\BanksException;
 use GuzzleHttp\Client;
+use stdClass;
 
 class Banks
 {
-	protected $client;
+    protected $bank;
+    protected $client;
 
-	function __construct()
-	{
-		$this->client = new Client();
-	}
+    protected $body = [];
+    protected $headers = [];
+    protected $options = [];
+
+    function __construct($bankUri)
+    {
+        $this->bank = new stdClass();
+
+        $this->bank->timezone = 'Asia/Jakarta';
+
+        $this->headers['Accept'] = 'application/json';
+        $this->headers['Content-Type'] = 'application/json';
+    }
+
+    function getBaseUri($baseUrl, $port = null)
+    {
+        $baseUrl .= ($port) ?? 443; //force to https
+
+        $this->client = new Client([
+            'base_uri' => $baseUrl
+        ]);
+    }
 }
